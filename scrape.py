127a0b1dddaf619bb9b92e36206cd0df1a0088fb
Omcevoy <omcevoy@uoregon.edu>
2019-10-11 13:32:21 -0700

    aesthetically updated output

--- scrape.py
+++ scrape.py
@@ -11,6 +11,10 @@ soup = BeautifulSoup(src)
 players = soup.find("tbody", class_= "statsTableContainer")
 leaderboard = str(players)
 leaderboard = leaderboard.split("</td>")
+
+print("\n\nPremier League All-Time Leading Scorers")
+print ("-" * 45)
+count = 1
 for piece in leaderboard:			#removes irrelevant lines
 	if "playerName" not in piece:
 		del(piece)
@@ -21,4 +25,5 @@ for piece in leaderboard:			#removes irrelevant lines
 				del(part)
 			else:
 				part = part.replace("</strong", "")
-				print part
+				print(str(count) + ". " +  part)
+				count += 1
