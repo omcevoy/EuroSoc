76d25a56ac56401af066e6f68ca46818b1b24700
Omcevoy <omcevoy@uoregon.edu>
2019-11-19 21:20:41 -0800

    For my 451 database processing class

--- scrape.py
+++ scrape.py
@@ -1,29 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+import unicodecsv as csv
+
+url = "https://www.spotrac.com/epl/afc-bournemouth/payroll/"
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text, 'lxml')
+
+data = [["Player", "Position", "Age", "Annual Salary", "Weekly Salary"]]
+rows = soup.find_all('tr')
+
+for tr in soup.find_all('tr'): #goes through tr elements
+    rowData = []
+    tdC = 0 #this will get rid of name duplicate
+    for td in tr.find_all('td'):
+        col = td.text.strip()
+        if (tdC == 0):
+            newCol = col.split('\n')
+            try:
+                rowData.append(newCol[1])
+            except:
+                continue
+        else:
+            rowData.append(col) #appends data to column
+        tdC +=1
+    data.append(rowData)
+
+with open("output.csv", 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerows(data)
 
 
-result = requests.get("https://www.premierleague.com/stats/top/players/goals")
-
-src = result.content
-
-soup = BeautifulSoup(src)
-
-players = soup.find("tbody", class_= "statsTableContainer")
-leaderboard = str(players)
-leaderboard = leaderboard.split("</td>")
-
-print("\n\nPremier League All-Time Leading Scorers")
-print ("-" * 45)
-count = 1
-for piece in leaderboard:			#removes irrelevant lines
-	if "playerName" not in piece:
-		del(piece)
-	else:
-		piece = piece.split(">")
-		for part in piece:
-			if "</strong" not in part:
-				del(part)
-			else:
-				part = part.replace("</strong", "")
-				print(str(count) + ". " +  part)
-				count += 1
